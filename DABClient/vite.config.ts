0ac467721ba90622669ca4242077da49e6c5bced
ArgentumRhodon <lac2559@g.rit.edu>
2025-08-17 10:58:25 -0400

    docker setup for new frontend

--- DABClient/vite.config.ts
+++ DABClient/vite.config.ts
@@ -4,4 +4,8 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
     plugins: [tailwindcss(), sveltekit()],
+    server: {
+        port: 3000,
+        strictPort: true,
+    },
 });
